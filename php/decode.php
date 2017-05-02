0f1820996da431d8986c98cc02e0a50626e3f2d6
Fernando Aparicio <faparicio@winworld.es>
2017-05-02 15:03:44 +0200

    Separate responsabilities

--- php/decode.php
+++ php/decode.php
@@ -8,7 +8,7 @@ $key = substr(sha1($key, true), 0, 16);
 
 $ciphertext = base64_decode($data_to_decrypt);
 
-$iv2 = substr($ciphertext2, 0, 16);
+$iv2 = substr($ciphertext, 0, 16);
 $ciphertext = substr($ciphertext, 16);
 //$original = openssl_decrypt($ciphertext, 'AES-128-CBC', $key2, OPENSSL_RAW_DATA, $iv2);
 $original = openssl_decrypt($ciphertext, 'AES-256-CBC', $key, OPENSSL_RAW_DATA, $iv2);
