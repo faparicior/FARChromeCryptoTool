7964e4a1c5e00d9a8a036cd6beec7cf78ebe5420
Fernando Aparicio <faparicior@gmail.com>
2017-06-13 21:48:15 +0200

    chrome extension options

--- public/options/options.js
+++ public/options/options.js
@@ -1,25 +1,44 @@
 $(function() {
-    $('#radioBtn a').on('click', function(){
-        var sel = $(this).data('title');
-        var tog = $(this).data('toggle');
-        $('#'+tog).prop('value', sel);
-        
-        $('a[data-toggle="'+tog+'"]').not('[data-title="'+sel+'"]').removeClass('active').addClass('notActive');
-        $('a[data-toggle="'+tog+'"][data-title="'+sel+'"]').removeClass('notActive').addClass('active');
-    })
 
-    
     var apiType = localStorage.getItem("apiType");
     var apiUrl = localStorage.getItem("apiUrl");
     var apiToken = localStorage.getItem("apiToken");
 
+    $('#radioBtn a').on('click', function(){
+        var sel = $(this).data('title');
+        var tog = $(this).data('toggle');
+        $('#'+tog).prop('value', sel);
+
+        localStorage.setItem("apiType", sel);
+
+        $('a[data-toggle="'+tog+'"]').not('[data-title="'+sel+'"]').removeClass('active').addClass('notActive');
+        $('a[data-toggle="'+tog+'"][data-title="'+sel+'"]').removeClass('notActive').addClass('active');
+    })
+
+    $('#radioBtn').find('[data-title="' + apiType + '"]').click();
+
     if (apiType) {
-        $("api-type").val(apiType);
+        $("#api-type").val(apiType);
     }
     if (apiUrl) {
-        $("api-url").val(apiUrl);
+        $("#api-url").val(apiUrl);
     }
     if (apiToken) {
-        $("api-token").val(apiToken);
+        $("#api-token").val(apiToken);
     }
+
+    $('#api-type').change(function(e){
+        console.log(e.target);
+        localStorage.setItem("apiType", $(e.target).val());
+    });
+
+    $('#api-url').change(function(e){
+        console.log(e.target);
+        localStorage.setItem("apiUrl", $(e.target).val());
+    });
+
+    $('#api-token').change(function(e){
+        console.log(e.target);
+        localStorage.setItem("apiToken", $(e.target).val());
+    });
 });
